6575454f48b5c9565ecf219be31a5e25380d98c2
Huong Bui-Vu <huong.buivu@gmail.com>
2023-03-24 00:36:05 +0400

    wip 3.27.23

---
+++ test.c
@@ -0,0 +1,49 @@
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// int main(void)
+// {
+// 	char *command = readline("tinyshell > ");
+// 	rl_replace_line("this is new", 0);
+// 	rl_redisplay();
+// 	rl_on_new_line();
+// 	add_history(command);
+// 	// printf("%s\n", command);
+// 	rl_clear_history(); //clear all history before exiting
+// 	free(command);
+// 	return (0);
+// }
+
+int main()
+{
+    // char *line;
+
+    // while ((line = readline(">tinyshell ")) != NULL) {
+    //     // Replace the line with a new string
+    //     // rl_replace_line("You entered: ", 1);
+    //     // rl_point = rl_end; // Move cursor to end of line
+    //     // rl_message("'%s'\n", line);
+    //     add_history(line);
+	// 	if (strcmp(line, "exit") == 0)
+	// 		break;
+	// 	if (line == ctrl C)
+	// 		//display new prompt
+    //     free(line);
+    // }
+	// free(line);
+	chdir("..");
+	char buf[10000];
+	printf("%s\n", getcwd(buf,1000));
+    return 0;
+}
+
+int main()
+{
+	chdir("..");
+	return (0);
+}
+
+
